ad8ec8aeb43b75f2fa6706e757660944ea3f83d0
George <gms1980@icloud.com>
2021-04-23 17:56:24 -0700

    built the database call

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3004;
@@ -6,8 +7,19 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-
-
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_tracker'
+    },
+    console.log('Connected to the employee_tracker database')
+)
+db.query(`SELECT * FROM Employee`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //Default response for any other request (NOT Found) this should be last placement or it will ove ride all others
