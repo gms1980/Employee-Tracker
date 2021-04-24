a728ee3d006eaa77a5feca3526a530b7ed256628
George <gms1980@icloud.com>
2021-04-24 12:23:38 -0700

    added put statement

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,14 +6,6 @@ INSERT INTO department (name) VALUES
 ("Finance"),
 ("Legal");
 
-INSERT INTO role (name, salary, department_id) VALUES 
-("Sales Lead", 100000, 1),
-("Salesperson", 80000, 1),
-("Software Engineer", 120000, 2),
-("Lead Engineer", 150000, 2),
-("Accountant", 125000, 3),
-("Lawyer", 190000, 4),
-
 INSERT INTO Employee (first_name, last_name, role_id, manager_id, department_id) VALUES
 ("John", "Doe", 1, 1),
 ("Mike", "Chan", 1, 2),
--- server.js
+++ server.js
@@ -109,6 +109,88 @@ app.post("/api/Employee", ({ body }, res) => {
   });
 });
 
+//Update a Employee's Department
+app.put("/api/Employee/:id", (req, res) => {
+  const errors = inputCheck(req.body, "department_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE Employee SET department_id = ? 
+                 WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Employee not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
+//ADDs Route for all Departments
+app.get("/api/department", (req, res) => {
+  const sql = `SELECT * FROM department`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+//Add Route for a single department
+app.get("/api/department/:id", (req, res) => {
+  const sql = `SELECT * FROM department WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+//Deletes a Department
+app.delete("/api/department/:id", (req, res) => {
+  const sql = `DELETE FROM department WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Department not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
 //Default response for any other request (NOT Found) this should be last placement or it will ove ride all others
 app.use((req, res) => {
   res.status(404).end();
