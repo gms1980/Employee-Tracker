4963b8429ec846dd749ac33cf6b8878550cbd966
George <gms1980@icloud.com>
2021-04-23 18:24:36 -0700

    added GET,DELETE,CREAT

--- server.js
+++ server.js
@@ -17,10 +17,38 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the employee_tracker database')
 )
-db.query(`SELECT * FROM Employee`, (err, rows) => {
-    console.log(rows);
-});
+// Get all the Employee
+// db.query(`SELECT * FROM Employee`, (err, rows) => {
+//     console.log(rows);
+// });
 
+// GET a single Employee
+// db.query(`SELECT * FROM Employee WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+//Delete a Employee
+// db.query(`DELETE FROM Employee WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create a candidate
+const sql = `INSERT INTO Employee (id, first_name, last_name, role_id, manager_id) 
+              VALUES (?,?,?,?,?)`;
+const params = [1, 'John', 'Doe', 1, 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 //Default response for any other request (NOT Found) this should be last placement or it will ove ride all others
 app.use((req, res) => {
